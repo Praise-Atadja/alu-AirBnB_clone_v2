4e88d5b295d3d19bae629129843fe32c01d900dd
Praise-Atadja <p.atadja@alustudent.com>
2023-02-23 09:49:40 +0300

    check code

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """This is the amenity class"""
-from models.base_model import BaseModel, Base
-from models.place import Place
-from sqlalchemy import Column, String, Table, ForeignKey
-from sqlalchemy.orm import relationship
 from os import getenv
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
 
 
 class Amenity(BaseModel, Base):
@@ -12,13 +11,9 @@ class Amenity(BaseModel, Base):
     Attributes:
         name: input name
     """
-    __tablename__ = "amenities"
-    name = Column(String(128),
-                  nullable=False)
-
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
     if getenv("HBNB_TYPE_STORAGE") == "db":
-        place_amenities = relationship(
-            "Place",
-            secondary="place_amenity",
-            viewonly=False,
-            back_populates="amenities")
+        place_amenities = relationship('Place',
+                                       secondary='place_amenity',
+                                       back_populates='amenities')
