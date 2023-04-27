2e449faa5efafaf46e4e4d4ae8a2410017b7750c
Praise-Atadja <106454335+Praise-Atadja@users.noreply.github.com>
2023-04-27 23:01:45 +0300

    Update 100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,13 +1,29 @@
-#/usr/bin/python3
-"""Module to execute functions"""
-
+#!/usr/bin/python3
+# Fabfile to delete out-of-date archives.
+import os
 from fabric.api import *
 
-env.host = ['35.231.167.55', '34.236.146.248']
+env.hosts = ["54.160.85.72", "35.175.132.106"]
+
 
 def do_clean(number=0):
-    """Function that deletes out-of-date archives"""
-    if number == 0 or number == 1:
-        
-    if number == 2:
+    """Delete out-of-date archives.
+    Args:
+        number (int): The number of archives to keep.
+    If number is 0 or 1, keeps only the most recent archive. If
+    number is 2, keeps the most and second-most recent archives,
+    etc.
+    """
+    number = 1 if int(number) == 0 else int(number)
+
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
+
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
         
